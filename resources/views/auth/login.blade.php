989fe3a2dcdbe8649304b67120942c46a79f2e54
中田佳孝 <a1086yn5974@gmail.com>
2021-11-21 15:06:56 +0900

    message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,18 +15,15 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" placeholder="メールアドレス" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
+                                placeholder="パスワード"
                                 required autocomplete="current-password" />
             </div>
 
@@ -46,7 +43,7 @@
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('ログイン') }}
                 </x-button>
             </div>
         </form>
